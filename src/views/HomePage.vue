5e95dda73c47818243a43ce98e49cd930a33f996
tang huayi <huayi.tang@etu.unistra.fr>
2023-02-26 16:15:02 +0100

    @ ├─.vscode                # vscode配置文件 |    └─extensions.json   # 项目推荐插件列表（可把项目中用到的vscode插件ID写进去，跑项目时没有安装这些插件会推荐安装） ├─public/                # 公共资源目录 ├─src/                   # 核心开发目录 |  ├─App.vue             # 项目根组件 |  ├─main.js             # 项目入口文件 |  ├─views/              # 项目视图目录 |  | └─Home/index.vue |  ├─stores/             # 统一状态管理目录-pinia |  ├─router/             # 项目路由目录 |  | └─index.js |  ├─components/         # 项目公共组件目录 |  ├─assets/             # 项目静态资源目录 ├─.eslintrc.cjs          # eslint 配置文件 ├─.gitignore             # git忽略文件 ├─.prettierrc.json       # prettier 配置文件 ├─README.md              # 项目说明文件 ├─index.html             # html入口文件 ├─package.json           # 项目配置和包管理文件 ├─vite.config.js         # vite 配置文件

---
+++ src/views/HomePage.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+
+<template>
+    <div class="home-page">
+        <h1>Home Page</h1>
+        <p>Home page content goes here.</p>
+    </div>
+</template>
+
+<style scoped></style>
